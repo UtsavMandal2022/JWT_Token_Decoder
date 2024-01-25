62bcce11fb8b0a31da1a03b6bae5cb2cbe5be72d
UtsavMandal2022 <utsav2016mandal@gmail.com>
2024-01-26 01:16:05 +0530

    C1

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import jwt
 
-def decode_jwt(token, secret='secret', algorithm='HS256'):
-    try:
-        decoded_data = jwt.decode(token, secret, algorithms=[algorithm])
-        return decoded_data
-    except jwt.ExpiredSignatureError:
-        print("Token has expired.")
-    except jwt.InvalidTokenError:
-        print("Invalid token.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+def decode_jwt(token,secret):
+    algos=['HS256', 'HS384', 'HS512', 'RS256', 'RS384', 'RS512', 'ES256', 'ES384', 'ES512', 'PS256', 'PS384', 'PS512']
+    for algo in algos:
+        try:
+            print(f"Now Trying {algo}...")
+            decoded_data = jwt.decode(token, secret, algorithms=[algo])
+            return decoded_data
+        except jwt.ExpiredSignatureError:
+            print("Token has expired.")
+        except jwt.InvalidTokenError:
+            print("Invalid token.")
+        except Exception as e:
+            print(f"An error occurred: {e}")
 
 # Example usage:
 token_to_decode = input("Enter token to decode: ")
-decoded_data = decode_jwt(token_to_decode)
-
+secret = input("Enter secret:( If you don't know the secret, just press enter)")
+if secret == "":
+    secret = 'secret'
+decoded_data = decode_jwt(token_to_decode,secret)
 if decoded_data:
     print("Decoded data:", decoded_data)
